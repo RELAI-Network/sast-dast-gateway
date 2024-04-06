6999966de0f26027cc7e62651970af2c5049f2f9
molgeek <74042620+morgueye4@users.noreply.github.com>
2024-04-06 12:29:12 +0000

    Recent scans endpoint

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ app.use(express.json());
 
 
 // MobSF API configurations
+const MOBSF_BASE_URL = process.env.MOBSF_BASE_URL;
 const MOBSF_API_KEY = process.env.MOBSF_API_KEY;
 
 const upload = multer({ storage: multer.memoryStorage() });
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 // New endpoint to receive file and call /api/v1/upload
 app.post('/upload', upload.single('file'), async (req, res) => {
@@ -24,10 +28,10 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     formData.append('file', req.file.buffer, req.file.originalname);
 
     try {
-        const response = await axios.post('http://localhost:3000/upload', formData, {
+        const response = await axios.post(MOBSF_BASE_URL+'/upload', formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
-                'Authorization': 'YOUR_API_KEY', // Replace with your actual API key
+                'Authorization': MOBSF_API_KEY, 
             },
         });
 
@@ -48,7 +52,24 @@ app.post('/scan', async (req, res) => {
     }
 
     try {
-        const response = await axios.post('http://localhost:3000/scan', { hash, re_scan }, {
+        const response = await axios.post(MOBSF_BASE_URL+'/scan', { hash, re_scan }, {
+            headers: {
+                'Authorization': MOBSF_API_KEY,
+            },
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error forwarding scan request:', error);
+        res.status(500).json({ error: 'Error forwarding scan request'});
+    }
+});
+
+// New endpoint to forward scan request
+app.get('/recent-scans', async (req, res) => {
+
+    try {
+        const response = await axios.get(MOBSF_BASE_URL+'/scans', {
             headers: {
                 'Authorization': MOBSF_API_KEY,
             },
@@ -61,63 +82,6 @@ app.post('/scan', async (req, res) => {
     }
 });
 
-
-/*
-// Mock MobSF API
-app.post('/scan-sim', async (req, res) => {
-    const apiKey = req.headers.authorization || req.headers['x-mobsf-api-key'];
-    const { hash, re_scan = 0 } = req.body;
-
-    if (!apiKey) {
-        return res.status(401).json({ error: 'Unauthorized: API key is required' });
-    }
-
-    if (!hash) {
-        return res.status(400).json({ error: 'Missing required parameter: hash' });
-    }
-
-    // Here, you can simulate any additional processing or validation
-    // based on the provided hash and re_scan parameters
-
-    res.json({
-        success: true,
-        message: 'Scan initiated successfully',
-        scanInfo: {
-            hash,
-            re_scan,
-        },
-    });
-});
-
-// Mock MobSF API
-app.post('/mobsf-sim', upload.single('file'), (req, res) => {
-    const apiKey = req.headers.authorization || req.headers['x-mobsf-api-key'];
-
-    if (!apiKey) {
-        return res.status(401).json({ error: 'Unauthorized: API key is required' });
-    }
-
-    if (!req.file) {
-        return res.status(400).json({ error: 'No file provided' });
-    }
-
-    const fileInfo = {
-        fileName: req.file.originalname,
-        fileSize: req.file.size,
-        fileType: req.file.mimetype,
-    };
-
-    // Here, you can simulate any additional processing or validation
-    // based on the uploaded file and API key
-
-    res.json({
-        success: true,
-        message: 'File uploaded successfully',
-        fileInfo,
-    });
-});
-
-*/
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
